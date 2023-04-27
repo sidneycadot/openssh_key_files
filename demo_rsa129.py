94b3f9f4a81c91e7f5c46d3bae52f4b6eb71cedc
Sidney Cadot <sidney@jigsaw.nl>
2023-04-27 12:37:08 +0200

    Cosmetic improvements.

--- demo_rsa129.py
+++ demo_rsa129.py
@@ -15,9 +15,10 @@ from rsa_math import modular_inverse, powermod
 def simple_string_to_int(s: str) -> int:
     """Apply the encoding described in the RSA-129 article."""
     result = 0
-    for c in s:
+    for c in s.lower():
         idx = " abcdefghijklmnopqrstuvwxyz".find(c)
-        assert idx != -1
+        if idx == -1:
+            raise ValueError("Unable to encode character {!r}.".format(c))
         result *= 100
         result += idx
     return result
@@ -32,118 +33,125 @@ def simple_int_to_string(m: int) -> str:
     return "".join(reversed(characters))
 
 
-# The original (secret) pair of prime factors.
-p = 3490529510847650949147849619903898133417764638493387843990820577
-q = 32769132993266709549961988190834461413177642967992942539798288533
+def main():
+    """Work through the example of the RSA-129 key discussed in Martin Gardner's Scientific American column."""
 
-print()
-print("=== GENERATING THE PUBLIC/PRIVATE KEY PAIR ===")
-print()
+    print()
+    print("=== GENERATING THE PUBLIC/PRIVATE KEY PAIR ===")
+    print()
 
-print("Secret primes, selected at random:")
-print()
-print("    p =", p)
-print("    q =", q)
-print()
+    # The original (secret) pair of prime factors.
+    p = 3490529510847650949147849619903898133417764638493387843990820577
+    q = 32769132993266709549961988190834461413177642967992942539798288533
 
-# The public key is (n, e).
-n = p * q  # The product of the two secret primes, given in the article.
-e = 9007   # The public exponent given in the article.
+    print("Secret primes, selected at random:")
+    print()
+    print("    p =", p)
+    print("    q =", q)
+    print()
 
-print("Public key:")
-print()
-print("    n =", n, "(p*q)")
-print("    e =", e, "(chosen public exponent)")
-print()
+    # The corresponding public key is (n, e).
+    n = p * q  # The product of the two secret primes, given in the article.
+    e = 9007   # The public exponent given in the article.
 
-# Only the person in possession of 'd' should be able to decode the message.
-#
-# The key property we need: for any M < n, M^(e*d) == M (mod n).
-#
-# Note: the original RSA description prescribes Euler's totient function rather than Carmichael's totient function.
-# Either will work; in fact, ANY multiple of Carmichael's lambda value will work, and Euler's totient is such a multiple.
+    print("Public key:")
+    print()
+    print("    n =", n, "(p*q)")
+    print("    e =", e, "(chosen public exponent)")
+    print()
 
-carmichael_lambda = math.lcm(p - 1, q - 1)
-euler_phi = (p - 1) * (q - 1)
+    # Only the party in possession of 'd' should be able to decode the message.
+    #
+    # The key property we need: for any M < n, M^(e*d) == M (mod n).
+    #
+    # Note: the original RSA description prescribes Euler's totient function rather than Carmichael's totient function.
+    # Either will work; in fact, ANY multiple of Carmichael's lambda value will work, and Euler's totient is such a multiple.
 
-print("Calculating the secret exponent d from p, q, and e:")
-print()
-print("    carmichael_lambda(n) =", carmichael_lambda)
-print("    euler_phi(n) =", euler_phi)
-print()
+    carmichael_lambda = math.lcm(p - 1, q - 1)
+    euler_phi = (p - 1) * (q - 1)
 
-#d = modular_inverse(e, euler_phi)
-d = modular_inverse(e, carmichael_lambda)
+    print("Calculating the secret exponent d from p, q, and e:")
+    print()
+    print("    carmichael_lambda(n) =", carmichael_lambda)
+    print("    euler_phi(n) =", euler_phi)
+    print()
 
-print("    d =", d)
-print()
+    #d = modular_inverse(e, euler_phi)
+    d = modular_inverse(e, carmichael_lambda)
 
-print("=== ENCRYPTION ===")
-print()
+    print("    d =", d)
+    print()
 
-# The secret message.
-plaintext_message_string = "the magic words are squeamish ossifrage"
+    print("=== ENCRYPTION ===")
+    print()
 
-plaintext_message_before_encryption = simple_string_to_int(plaintext_message_string)
+    # The secret message.
+    plaintext_message_string = "the magic words are squeamish ossifrage"
 
-print("Plaintext message before encryption:")
-print()
-print("   ", repr(plaintext_message_string))
-print("   ", plaintext_message_before_encryption)
-print()
+    plaintext_message_before_encryption = simple_string_to_int(plaintext_message_string)
 
-# The "ciphertext message" for the RSA-129 challenge is given in the article as a decimal integer.
-ciphertext_message = powermod(plaintext_message_before_encryption, e, n)
+    print("Plaintext message before encryption:")
+    print()
+    print("   ", repr(plaintext_message_string))
+    print("   ", plaintext_message_before_encryption)
+    print()
 
-print("Ciphertext message (after encryption using n and e):")
-print()
-print("   ", ciphertext_message)
-print()
+    # The "ciphertext message" for the RSA-129 challenge is given in the article as a decimal integer.
+    ciphertext_message = powermod(plaintext_message_before_encryption, e, n)
 
-print("=== DECRYPTION ===")
-print()
+    print("Ciphertext message (after encryption using n and e):")
+    print()
+    print("   ", ciphertext_message)
+    print()
 
-plaintext_message_after_decryption = powermod(ciphertext_message, d, n)
+    print("=== DECRYPTION ===")
+    print()
 
-print("Plaintext message (after decription using n and d):")
-print()
-print("   ", plaintext_message_after_decryption)
-print("   ", repr(simple_int_to_string(plaintext_message_after_decryption)))
-print()
+    plaintext_message_after_decryption = powermod(ciphertext_message, d, n)
 
-print("=== SIGNATURE CHECKING ===")
-print()
+    print("Plaintext message (after decryption using n and d):")
+    print()
+    print("   ", plaintext_message_after_decryption)
+    print("   ", repr(simple_int_to_string(plaintext_message_after_decryption)))
+    print()
 
-# Both this "signature" number and its decrypted version are given in the rticle.
+    print("=== SIGNATURE CHECKING ===")
+    print()
 
-plaintext_signature_string = "first solver wins one hundred dollars"
+    # Both this "signature" number and its decrypted version are given in the rticle.
 
-plaintext_signature_before_encryption = simple_string_to_int(plaintext_signature_string)
+    plaintext_signature_string = "first solver wins one hundred dollars"
 
-print("Plaintext signature before encryption:")
-print()
-print("   ", repr(plaintext_signature_string))
-print("   ", plaintext_signature_before_encryption)
-print()
+    plaintext_signature_before_encryption = simple_string_to_int(plaintext_signature_string)
 
-ciphertext_signature = powermod(plaintext_signature_before_encryption, d, n)
+    print("Plaintext signature before encryption:")
+    print()
+    print("   ", repr(plaintext_signature_string))
+    print("   ", plaintext_signature_before_encryption)
+    print()
 
-# The article gives this 128-digit encrypted signature:
-#
-#   16717861150380844246015271389168\
-#   39824543690103235831121783503844\
-#   69290626554487922371144905095786\
-#   08655662496577974840004057020373
+    ciphertext_signature = powermod(plaintext_signature_before_encryption, d, n)
 
-print("Ciphertext signature (after encryption using n and d):")
-print()
-print("   ", ciphertext_signature)
-print()
+    # The article gives this 128-digit encrypted signature:
+    #
+    #   16717861150380844246015271389168\
+    #   39824543690103235831121783503844\
+    #   69290626554487922371144905095786\
+    #   08655662496577974840004057020373
 
-plaintext_signature_after_decryption = powermod(ciphertext_signature, e, n)
+    print("Ciphertext signature (after encryption using n and d):")
+    print()
+    print("   ", ciphertext_signature)
+    print()
 
-print("Signature (after decryption using n and e):")
-print()
-print("   ", plaintext_signature_after_decryption)
-print("   ", repr(simple_int_to_string(plaintext_signature_after_decryption)))
-print()
+    plaintext_signature_after_decryption = powermod(ciphertext_signature, e, n)
+
+    print("Signature (after decryption using n and e):")
+    print()
+    print("   ", plaintext_signature_after_decryption)
+    print("   ", repr(simple_int_to_string(plaintext_signature_after_decryption)))
+    print()
+
+
+if __name__ == "__main__":
+    main()
